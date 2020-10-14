09cdebde550a299409989c130868b12b4e007629
smtrf <71617037+smtrf@users.noreply.github.com>
2020-10-14 16:15:07 -0500

    Add files via upload

---
+++ TANSIE_Hey_Day.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#Prompting student to enter their name in the terminal
+echo "Please enter your name"
+
+#Shell program reading student's name
+read name
+
+#Shell program printing student's name in standard out
+echo "Hey $name!"
